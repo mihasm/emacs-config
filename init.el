5ca77eb094542be75a82a1e3378da532f2498ea8
Miha Smrekar <xmiha.xsmrekar@gmail.com>
2024-10-03 10:49:06 +0200

    added all packages to use-package. fixed theme loading

--- init.el
+++ init.el
@@ -1,16 +1,23 @@
+;; Load custom settings from a separate file
+(setq custom-file (concat user-emacs-directory "custom.el"))
+
+;; If the custom settings file exists, load it
+(when (file-exists-p custom-file)
+  (load custom-file))
+
 ;; Remove the startup screen and scratch message
 (setq inhibit-startup-message t)
 (setq initial-scratch-message nil)
 
-;; Remove the menu bar
-(if (fboundp 'scroll-bar-mode)                                                 
-    (scroll-bar-mode -1)) 
-(if (fboundp 'menu-bar-mode)
-    (menu-bar-mode -1))
-(if (fboundp 'set-fringe-mode)
-    (set-fringe-mode 0))
+;; Remove UI elements
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'set-fringe-mode) (set-fringe-mode 0))
 (tool-bar-mode -1)
 (icomplete-mode 1)
+
+;; Auto-revert buffers when files change externally
+(global-auto-revert-mode t)
 
 ;; Set initial mode to text-mode
 (setq initial-major-mode 'text-mode)
@@ -26,28 +33,29 @@
   (package-install 'use-package))
 (require 'use-package)
 
-;; Enable drag-stuff package
-(use-package drag-stuff
-  :ensure t
-  :config
-  (drag-stuff-global-mode 1)
-  ;; Set keyboard shortcuts for drag-stuff
-  (global-set-key (kbd "ESC <down>") 'drag-stuff-down)
-  (global-set-key (kbd "M-<down>") 'drag-stuff-down)
-  (global-set-key (kbd "ESC <up>") 'drag-stuff-up)
-  (global-set-key (kbd "M-<up>") 'drag-stuff-up)
-  (global-set-key (kbd "ESC <left>") 'drag-stuff-left)
-  (global-set-key (kbd "M-<left>") 'drag-stuff-left) 
-  (global-set-key (kbd "ESC <right>") 'drag-stuff-right)
-  (global-set-key (kbd "M-<right>") 'drag-stuff-right))
+;; Auto-save, backup, and lock file configuration
+(setq backup-directory (concat user-emacs-directory "backups/"))
+(setq auto-save-directory (concat user-emacs-directory "auto-saves/"))
+(setq lock-file-directory (concat user-emacs-directory "lock-files/"))
+(unless (file-exists-p backup-directory)
+  (make-directory backup-directory t))
+(unless (file-exists-p auto-save-directory)
+  (make-directory auto-save-directory t))
+(unless (file-exists-p lock-file-directory)
+  (make-directory lock-file-directory t))
+(setq backup-directory-alist `(("." . ,backup-directory)))
+(setq auto-save-file-name-transforms `((".*" ,auto-save-directory t)))
+(setq lock-file-name-transforms `((".*" ,lock-file-directory t)))
 
-; sublime text theme
-(load-theme 'monokai t)
+;; Theme and Font
+(use-package monokai-theme
+  :ensure t)
+
 (set-face-attribute 'default nil
-                    :family "Menlo" ;; Replace with your preferred font
-                    :height 120) ;; Adjust size (in 1/10pt, so 120 = 12pt)
+                    :family "Menlo"
+                    :height 120)
 
-; disable arrow keys for movement
+;; Disable arrow keys for movement
 (global-unset-key (kbd "<left>"))
 (global-unset-key (kbd "<right>"))
 (global-unset-key (kbd "<up>"))
@@ -56,92 +64,98 @@
 (global-unset-key (kbd "<C-right>"))
 (global-unset-key (kbd "<C-up>"))
 (global-unset-key (kbd "<C-down>"))
-;(global-unset-key (kbd "<M-left>"))
-;(global-unset-key (kbd "<M-right>"))
-;(global-unset-key (kbd "<M-up>"))
-;(global-unset-key (kbd "<M-down>"))
 
-; move temporary emacs files to ~/.emacs-saves and ~/.emacs-lockfiles
-(setq backup-directory-alist '(("." . "~/.emacs-saves")))                                
-(setq auto-save-file-name-transforms '((".*" "~/.emacs-saves" t)))
-(setq lock-file-name-transforms `((".*" "~/.emacs-lockfiles/" t)))
+;; drag-stuff package
+(use-package drag-stuff
+  :ensure t
+  :config
+  (drag-stuff-global-mode 1)
+  (global-set-key (kbd "ESC <down>") 'drag-stuff-down)
+  (global-set-key (kbd "M-<down>") 'drag-stuff-down)
+  (global-set-key (kbd "ESC <up>") 'drag-stuff-up)
+  (global-set-key (kbd "M-<up>") 'drag-stuff-up)
+  (global-set-key (kbd "ESC <left>") 'drag-stuff-left)
+  (global-set-key (kbd "M-<left>") 'drag-stuff-left)
+  (global-set-key (kbd "ESC <right>") 'drag-stuff-right)
+  (global-set-key (kbd "M-<right>") 'drag-stuff-right))
 
-; detect filechanges outside of emacs and update buffer if change detected
-(global-auto-revert-mode t)
+;; Obsidian package
+(use-package obsidian
+  :ensure t
+  :config
+  (obsidian-specify-path "~/Proton Drive/Obsidian")
+  (setq obsidian-wiki-link-create-file-in-inbox nil)
+  (setq obsidian-daily-notes-directory "Zapiski")
+  (global-obsidian-mode t)
+  (add-hook 'obsidian-mode-hook
+            (lambda ()
+              (local-set-key (kbd "C-c C-o") 'obsidian-follow-link-at-point)
+              (local-set-key (kbd "C-c C-l") 'obsidian-insert-wikilink)
+              (local-set-key (kbd "C-c C-b") 'obsidian-backlink-jump))))
 
-(require 'obsidian)
-(obsidian-specify-path "~/Proton Drive/Obsidian")
-;; Clicking on a wiki link referring a non-existing file the file can be
-;; created in the inbox (t) or next to the file with the link (nil).
-;; Default: t - creating in the inbox
-(setq obsidian-wiki-link-create-file-in-inbox nil)
-;; You may want to define a folder for daily notes. By default it is the inbox.
-(setq obsidian-daily-notes-directory "Zapiski")
-;; Directory of note templates, unset (nil) by default
-;(setq obsidian-templates-directory "Templates")
-;; Daily Note template name - requires a template directory. Default: Daily Note Template.md
-;(setq obsidian-daily-note-template "Daily Note Template.md")
-
-
-;; Define obsidian-mode bindings
-(add-hook
- 'obsidian-mode-hook
- (lambda ()
-   ;; Replace standard command with Obsidian.el's in obsidian vault:
-   (local-set-key (kbd "C-c C-o") 'obsidian-follow-link-at-point)
-
-   ;; Use either `obsidian-insert-wikilink' or `obsidian-insert-link':
-   (local-set-key (kbd "C-c C-l") 'obsidian-insert-wikilink)
-
-   ;; Following backlinks
-   (local-set-key (kbd "C-c C-b") 'obsidian-backlink-jump)))
-
-;; Optionally you can also bind a few functions:
-;; replace "YOUR_BINDING" with the key of your choice:
-;; (global-set-key (kbd "YOUR_BINDING") 'obsidian-jump)       ;; Opening a note
-;; (global-set-key (kbd "YOUR_BINDING") 'obsidian-capture)    ;; Capturing a new note in the inbox
-;; (global-set-key (kbd "YOUR_BINDING") 'obsidian-daily-note) ;; Creating daily note
-
-;; Activate detection of Obsidian vault
-(global-obsidian-mode t)
-
-
-;; Multiple cursors plugin
+;; Multiple cursors
 (use-package multiple-cursors
-  :ensure t)
+  :ensure t
+  :bind (("C-d" . mc/mark-next-like-this)
+         ("C-c C-d" . mc/mark-all-like-this)))
 
-(global-set-key (kbd "C-d") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-c C-d") 'mc/mark-all-like-this)
+(defun force-global-C-d ()
+  (local-set-key (kbd "C-d") 'mc/mark-next-like-this))
+(add-hook 'after-change-major-mode-hook 'force-global-C-d)
 
-;; Sync environment with shell
+;; Company mode
+(use-package company
+  :ensure t
+  :hook (after-init . global-company-mode))
+
+;; Shell mode shortcuts
+(add-hook 'shell-mode-hook
+          (lambda ()
+            (define-key shell-mode-map (kbd "<up>") 'comint-previous-input)
+            (define-key shell-mode-map (kbd "<down>") 'comint-next-input)))
+
+;; exec-path-from-shell
 (use-package exec-path-from-shell
   :ensure t
   :config
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize)))
 
-;; Enable company mode for all buffers
-(add-hook 'after-init-hook 'global-company-mode)
+;; C-mode configuration
+(defun set-compile-command-for-c ()
+  (when (eq major-mode 'c-mode)
+    (let* ((file (file-name-nondirectory buffer-file-name))
+           (output-file (file-name-sans-extension file)))
+      (setq compile-command (format "gcc %s -o %s" file output-file)))))
+(add-hook 'c-mode-hook 'set-compile-command-for-c)
 
-;; shell mode prev and next command shortcuts
-(add-hook 'shell-mode-hook                                                          
-(lambda ()                                                                
-  (define-key shell-mode-map (kbd "<up>") 'comint-previous-input)         
-  (define-key shell-mode-map (kbd "<down>") 'comint-next-input))) 
+;; ggtags for navigation in C/C++
+(use-package ggtags
+  :ensure t
+  :hook ((c-mode c++-mode) . ggtags-mode))
+(global-set-key (kbd "M-.") 'ggtags-find-definition)
+(global-set-key (kbd "M-,") 'pop-tag-mark)
+(setq gtags-suggested-key-mapping t)
+(setq ggtags-include-directories '("/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"))
+(global-set-key (kbd "C-c g") (lambda () (interactive) (shell-command "gtags --incremental")))
 
-;; Custom-set variables and faces generated by Emacs
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("5c5de678730ceb4e05794431dd65f30ffe9f1ed6c016fa766cdf909ba03e4df4" "c7fd1708e08544d1df2cba59b85bd25263180b19b287489d4f17b9118487e718" default))
- '(package-selected-packages
-   '(exec-path-from-shell multiple-cursors obsidian monokai-theme company drag-stuff)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Disable mouse interactions globally
+(use-package disable-mouse
+  :ensure t
+  :config
+  (global-disable-mouse-mode))
+
+;; Poet theme
+(use-package poet-theme
+  :ensure t
+  ;:config
+  ;(load-theme 'poet t)
+  ;; Enable variable-pitch-mode in text modes (org-mode, markdown-mode, etc.)
+  ;(add-hook 'text-mode-hook
+  ;          (lambda ()
+  ;            (variable-pitch-mode 1)))
+  ;; Set custom fonts for different text faces
+  ;(set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 130)
+  ;(set-face-attribute 'fixed-pitch nil :family "DejaVu Sans Mono")
+  ;(set-face-attribute 'variable-pitch nil :family "IBM Plex Serif")
+  )
